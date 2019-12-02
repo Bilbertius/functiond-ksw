3e1ba969f29bebf54dfc1165557251261e780ace
Bilbertius <lauchlinmacneil@gmail.com>
2019-12-02 14:32:21 -0700

    Return greeting rather than log

--- drills.js
+++ drills.js
@@ -1,7 +1,10 @@
 "use strict";
 
 function createGreeting() {
-  console.log("Hi, my name is Will");
+  return "Hi, my name is Will";
 }
 
 createGreeting();
+
+const greeting1 = createGreeting();
+console.log(greeting1);
