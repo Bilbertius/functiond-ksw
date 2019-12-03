8f68c5d1955906e45f6bfc372ac32cf6a48bcbb4
KRAIG WILLIAMS <williamskrg@gmail.com>
2019-12-03 01:22:43 -0400

    finish jediName and Infinity

--- drills2.js
+++ drills2.js
@@ -6,4 +6,25 @@ const jediName = (firstName, lastName) =>{
 }
 
 
-jediName("Beyonce", "Knowles")
+//jediName("Kraig", "Williams")
+
+const beyond = num =>{
+   if(num === Infinity || num === Number.NEGATIVE_INFINITY){
+console.log("And beyond")
+}
+else if ( Number.isFinite(num) && Math.sign(num) ==1 ){
+    console.log("To infinity");
+}
+else if (Number.isFinite(num) && Math.sign(num) == -1){
+    console.log("To negative infinity")
+}
+else if (num ===0){
+    console.log("Staying home")
+}
+
+} 
+beyond(0);
+
+
+
+
